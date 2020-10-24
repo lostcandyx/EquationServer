b772266f0f4729ff88f01f49e3053da347dc019e
lostcandyx <515586472@qq.com>
2020-10-24 19:27:25 +0800

    Unable to use Cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
-#include "EquationSolver.h"
-#include "Hexagon.h"
+#include <iostream>
+#include "Specifications.h"
 
 int main(int argc, char const *argv[])
 {
-    Hexagon P;
-    // Point3D A(1,2,3);
-    
+    //Point3D A(1,2,3);
     // P = A*3.0;
     std::cout<<PI<<std::endl;
 
